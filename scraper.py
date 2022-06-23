55dfb4cad01acf97e313e5fa9d03f284dadc8046
Pratiksha <pratiksha@192.168.1.104>
2022-06-23 17:26:34 +0100

    small changes

--- scraper.py
+++ scraper.py
@@ -139,7 +139,7 @@ class Scraper():
         return None
 
 if __name__ == "__main__":
-    scraper = Scraper("pratikshavyas93@gmail.com","**********")
+    scraper = Scraper("your username","your password")
     scraper.load_page('https://www.linkedin.com')
     get_all_topbar_url_name = scraper.select_options_url()
     for url_name, url in get_all_topbar_url_name:
